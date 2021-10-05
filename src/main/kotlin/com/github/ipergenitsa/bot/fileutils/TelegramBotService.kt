c5c70a77d2bacf87556b245116c2867a757e0147
Igor P <ipergenitsa@gmail.com>
2021-10-05 22:52:13 +0300

    Stop the bot gracefully

--- src/main/kotlin/com/github/ipergenitsa/bot/fileutils/TelegramBotService.kt
+++ src/main/kotlin/com/github/ipergenitsa/bot/fileutils/TelegramBotService.kt
@@ -1,11 +1,28 @@
 package com.github.ipergenitsa.bot.fileutils
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.DisposableBean
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.generics.BotSession
 
 @Service
-class TelegramBotService(telegramBotsApi: TelegramBotsApi, fileCompressionBot: FileCompressionBot) {
+class TelegramBotService(telegramBotsApi: TelegramBotsApi, fileCompressionBot: FileCompressionBot): DisposableBean {
+
+    private val botSession: BotSession
+
     init {
-        telegramBotsApi.registerBot(fileCompressionBot)
+        botSession = telegramBotsApi.registerBot(fileCompressionBot)
+    }
+
+    override fun destroy() {
+        log.info("Stopping The Bot...")
+        botSession.stop()
+        log.info("The Bot has been stopped")
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(TelegramBotService::class.java)
     }
 }
